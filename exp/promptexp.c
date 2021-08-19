b5fd9d487f429bbb67ee86bcc6554f5cb6ab51a6
krummitz <amidbidee@gmail.com>
2021-08-19 08:08:27 +0100

    added comment details to promt source

--- exp/promptexp.c
+++ exp/promptexp.c
@@ -2,46 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * inputPrompt - prints a prompt and fills a argument array
+ * @args: pointer to args pointer
+ * Return: array length
+ */
 
-int inputprompt(char **args)
+int inputPrompt(char **args)
 {
-    char *line = NULL, *str, d[] = " ", l_len;
-    size_t len = 0;
-    ssize_t nr;
-    int i = 0, j;
+    char *line = NULL, *str, d[] = " ", l_len; /* a lineptr, string ptr, array delimeter, array length */
+    size_t len = 0; /* var for length of each strings */
+    ssize_t nr; /* var for number of line to be read */
+    int i = 0, j; /* var for indexes */
 
-    printf("$");
-    nr = getline(&line, &len, stdin);
+    printf("$");  /* prints a dollar sign before commands everytime it's called */
+    nr = getline(&line, &len, stdin);  /* get the commands from standard input */
 
-    if (nr == -1)
+    if (nr == -1)   /* if it fails to read the lines, exit */
         exit(EXIT_FAILURE);
 
     if (nr)
     {
-        str = strtok(line, d);
+        str = strtok(line, d);  /* create and extract each token to str */
         while (str != NULL)
         {
-            args[l_len] = str;   
-            str = strtok(NULL, d);
-            l_len++;
+            args[l_len] = str;   /* fill the args array with the tokens */
+            str = strtok(NULL, d);  /* Not to sure but i think this separates each token in array */
+            l_len++;    /* increase this so we can use as length */
         }
     }
 
-    if (args == NULL)
+    if (args == NULL)  /* if the args wasn't filled exit */
         exit (EXIT_FAILURE);
 
-    return (l_len);
+    return (l_len); 
 }
 
 int main()
 {
-    int i = 0, line_len;
+    int i = 0, line_len=0, o_l;
     char *command[1024];
 
     while (1)
     {
-        memset(command, 0, 1024);
-        line_len = inputprompt(command);
+        memset(command, 0, 1024); /* the array keeps allocating, so this clears it */
+        line_len = inputPrompt(command); /* the prompt returns the length of array after this*/
+
+        /* THIS PART WAS FOR THE SAKE OF TESTING */
+        printf("array length-> %d", line_len); /**
+                                                but it adds the length of current array to
+                                                previous length, please fix if you can
+                                                */
         while (i < line_len)
         {
             if (command[i] != NULL)
